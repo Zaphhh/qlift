b90cf8d2a052d1885308c4a2d7ea35dc0eba6a59
matuadoc <88599935+MatuaDoc@users.noreply.github.com>
2023-07-27 22:00:40 +1200

    Add flags to compile properly on macOS
    This is required because Qt 6 lacks pkg-config
    files in Homebrew.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,34 @@
 // swift-tools-version:5.8
 import PackageDescription
 
+var prefix: String = ""
+#if (arch(x86_64))
+prefix = "/usr/local"
+#else
+prefix = "/opt/homebrew"
+#endif
+
+let cxxSettings: [CXXSetting] = [
+    .unsafeFlags([
+            "-I\(prefix)/opt/qt@6/lib/QtCore.framework/Headers",
+            "-I\(prefix)/opt/qt@6/lib/QtGui.framework/Headers",
+            "-I\(prefix)/opt/qt@6/lib/QtWidgets.framework/Headers",
+            "-I\(prefix)/opt/qt@6/include"
+        ],
+        .when(platforms: [.macOS])
+    )
+]
+
+let linkerSettings: [LinkerSetting] = [
+    .unsafeFlags([
+            "\(prefix)/opt/qt@6/lib/QtCore.framework/QtCore",
+            "\(prefix)/opt/qt@6/lib/QtGui.framework/QtGui",
+            "\(prefix)/opt/qt@6/lib/QtWidgets.framework/QtWidgets",
+        ],
+        .when(platforms: [.macOS])
+    )
+]
+
 let package = Package(
     name: "Qlift",
     products: [
@@ -8,8 +36,14 @@ let package = Package(
         .library(name: "Qlift", targets: ["Qlift"]),
     ],
     targets: [
-        .target(name: "Qlift", dependencies: ["CQlift"]),
-        .target(name: "CQlift", dependencies: ["CQt6Widgets"]),
+        .target(name: "Qlift", dependencies: ["CQlift"],
+            cxxSettings: cxxSettings,
+            linkerSettings: linkerSettings
+        ),
+        .target(name: "CQlift", dependencies: ["CQt6Widgets"],
+            cxxSettings: cxxSettings,
+            linkerSettings: linkerSettings
+        ),
         .systemLibrary(name: "CQt6Widgets", pkgConfig: "Qt6Widgets"),
         .executableTarget(name: "qlift-uic")
     ],
